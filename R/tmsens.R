bb01a392717827661af810593a5539841873ba89
Tom Palmer <remlapmot@hotmail.com>
2021-02-11 10:48:57 +0000

    start package level helpfile

---
+++ R/tmsens.R
@@ -0,0 +1,8 @@
+#' tmsens: Sensitivity Analysis Using the Trimmed Mean Estimator
+#'
+#' This package implements sensitivity analysis using the trimmed mean estimator.
+#'
+#' @name tmsens-package
+#' @aliases tmsens
+#' @docType package
+NULL
