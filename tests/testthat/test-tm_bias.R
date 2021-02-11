8f971a76b7b75ee117f57684b7f8e3c8531b4b06
dea-hazewinkel <78436987+dea-hazewinkel@users.noreply.github.com>
2021-02-11 15:29:44 +0000

    tm_bias unit testing

--- tests/testthat/test-tm_bias.R
+++ tests/testthat/test-tm_bias.R
@@ -1,11 +1,67 @@
 context("tm_bias")
 
-#test_dat <- as.data.frame(cbind(c(rep(0,500),rep(1,500)),
-#c(sort(rnorm(500,0,1)),sort(rnorm(500,1,1.5))),
-#rbinom(1000,2,0.4), rnorm(1000,0,1)))
-#colnames(test_dat) <- c("TR", "Y", "U", "U2")
-#test_dat$Y[which(test_dat$TR==0)[1:150]] <- NA
-#test_dat$Y[which(test_dat$TR==1)[sample(seq(1,400),200, replace=FALSE)]] <- NA
-#out1 <- tm(Y ~ TR + U + U2, GR="TR", trF=0.3, side="LOW", n_perm=1000, adj_est=FALSE, data=test_dat)
-#out1$trimfrac
-#sum(is.na(test_dat$Y[test_dat$TR==0]))/length(which(test_dat$TR==0))
+set.seed(4077)
+B_test_dat <- as.data.frame(cbind(c(rep(0,500),rep(1,500)),
+                                c(sort(rnorm(500,0,1)),sort(rnorm(500,1,1.5)))))
+colnames(B_test_dat) <- c("TR", "Y")
+
+B_test_dat$Y[which(B_test_dat$TR==0)[1:150]] <- NA
+B_test_dat$Y[which(B_test_dat$TR==1)[sample(seq(1,400),
+                                        200, replace=FALSE)]] <- NA
+
+B_test_dat2 <- B_test_dat; B_test_dat2$Y[1:10] <- "Oops"
+B_test_dat3 <- B_test_dat; B_test_dat3$TR[1:10] <- 3
+
+
+
+# checking total bias
+expect_equal(round(as.numeric(tm_bias(formula= Y ~ TR, "TR", trF=0.5,
+                         side="LOW", spread_TG=0.4,
+                         spread_CG=0.8, data=B_test_dat)$total_bias),4),
+             round(1.634035,4))
+
+# checking TM estimate
+expect_equal(round(as.numeric(tm_bias(formula= Y ~ TR, "TR", trF=0.5,
+                                      side="LOW", spread_TG=0.4,
+                                      spread_CG=0.8, data=B_test_dat)$TM_estimate),4),
+             round(1.204756,4))
+
+# checking total bias under maximal violation of strong MNAR assumption in CG group
+expect_equal(round(as.numeric(tm_bias(formula= Y ~ TR, "TR", trF=0.5,
+                                      side="LOW", spread_TG=0.4,
+                                      spread_CG=0.8, data=B_test_dat)$max_bias_CG[4]),4),
+             round(-0.7331188,4))
+
+
+# checking errors
+expect_error(tm_bias(formula= Y ~ TR, "Trt", trF=0.5,
+                                      side="LOW", spread_TG=0.4,
+                                      spread_CG=0.8, data=B_test_dat),
+             "TR variable not in data")
+
+expect_error(tm_bias(formula= Y ~ TR, "TR", trF=0.5,
+                     side="LOW", spread_TG=0.4,
+                     spread_CG=0.8, data=B_test_dat2),
+             "Y non-numeric")
+
+expect_error(tm_bias(formula= Y ~ TR, "TR", trF=0.5,
+                     side="LOW", spread_TG=0.4,
+                     spread_CG=0.8, data=B_test_dat3),
+             "TR non-binary")
+
+expect_error(tm_bias(formula= Y ~ TR, "TR", trF=0.5,
+                     side="LOW", spread_TG=0.4,
+                     spread_CG=0.2, data=B_test_dat),
+             "Comparator Gr spread smaller than dropout proportion")
+
+expect_error(tm_bias(formula= Y ~ TR, "TR", trF=0.5,
+                     side="LOW", spread_TG=0.1,
+                     spread_CG=0.8, data=B_test_dat),
+             "Treatment Gr spread smaller than dropout proportion")
+
+expect_error(tm_bias(formula= Y ~ TR, "TR", trF=0.35,
+                     side="LOW", spread_TG=0.4,
+                     spread_CG=0.3, data=B_test_dat),
+             "Trimming fraction smaller than largest dropout proportion")
+
+
